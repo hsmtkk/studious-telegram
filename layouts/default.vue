24130911bdc1b7be9a55e1c89612a12a03e58b6d
hsmtkk <hsmtkk@gmail.com>
2023-12-30 18:56:38 +0900

    basic structure

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-title>Reading recorder</v-app-bar-title>
+        </v-app-bar>
+        <v-main>
+            <slot />
+        </v-main>
+    </v-app>
+</template>
